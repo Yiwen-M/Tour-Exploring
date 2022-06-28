e1db84e3eb7ec04deef86b39a3f5837d6b417d7b
yww0225 <yw0225.career@gmail.com>
2022-06-28 09:39:59 -0700

    connecting db, creating tour model and creating documents to test the model

--- server.js
+++ server.js
@@ -1,7 +1,54 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    userFindAndModify: false,
+  })
+  .then(() => console.log('DB connction successful'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour mush have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  rating: 4.7,
+  price: 497,
+});
+
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR ❌', err);
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
