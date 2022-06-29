f7a8287ca8e511859e7a341c4eecdcc77e30b870
yww0225 <yw0225.career@gmail.com>
2022-06-29 16:37:10 -0700

    creating virtual property: duration week, implementing middleware function: document, query and aggregation and validating the data

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -155,7 +155,7 @@ exports.getMonthlyPlan = async (req, res) => {
         $match: {
           startDates: {
             $gte: new Date(`${year}-01-01`),
-            $lte: new Date(`${year}-12-31`),
+            // $lte: new Date(`${year}-12-31`),
           },
         },
       },
